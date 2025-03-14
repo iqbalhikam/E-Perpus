a4377c4989138aecffdb8d1b1675f5478bcc2be5
iqbal hikam <idiscrus@gmail.com>
2025-03-14 19:08:59 +0700

    app modifire

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -6,31 +6,39 @@ import { generateFromEmail } from "unique-username-generator";
 
 export const authRouter = createTRPCRouter({
   register: publicProcedure
+  
+    // validate input users
     .input(
       z.object({
         email: z.string().email().toLowerCase(),
         password: passwordSchema,
       }),
     )
+
+    // add data to supabase
     .mutation(async ({ ctx, input }) => {
       const { db } = ctx;
       const { email, password } = input;
 
       await db.$transaction(async (tx) => {
-        let userId = ""
+        let userId = "";
 
         try {
+
+          // create auth users
           const { data, error } =
             await supabaseAdminClient.auth.admin.createUser({
               email,
               password,
             });
           if (data.user) {
-            userId = data.user.id
+            userId = data.user.id;
           }
           if (error) throw error;
 
           const generatedUsername = generateFromEmail(email);
+
+          // add profile users
           await tx.profile.create({
             data: {
               email,
@@ -40,7 +48,7 @@ export const authRouter = createTRPCRouter({
           });
         } catch (error) {
           console.log(error);
-          await supabaseAdminClient.auth.admin.deleteUser(userId)
+          await supabaseAdminClient.auth.admin.deleteUser(userId);
         }
       });
     }),
